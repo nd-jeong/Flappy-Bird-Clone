8e9b1dd92f16c0c08f95566580ebed23ce469a5a
Andy Jeong <unjin89@gmail.com>
2019-05-06 16:48:31 -0400

    added sprites early to help visualize where objects are on screen

--- js/app.js
+++ js/app.js
@@ -4,11 +4,26 @@ let birdX = 10;
 let birdY = 360;
 let gravity = 2;
 
-let 
+const pipes = [];
+
+const bird = new Image();
+bird.src = 'images/bluebird-upflap.png';
+const pipeTop = new Image();
+pipeTop.src = 'images/pipe-green-top.png';
+const pipeBottom = new Image();
+pipeBottom.src = 'images/pipe-green-bottom.png';
+const bg = new Image();
+bg.src = 'images/background-night.png';
 
 const drawMap = () => {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(bird, birdX, birdY);
+    ctx.drawImage(pipeTop, 100, 0, 100, 350);
+    ctx.drawImage(pipeBottom, 100, 500, 100, 350);
+    
+    requestAnimationFrame(drawMap);
 }
 
 drawMap();
