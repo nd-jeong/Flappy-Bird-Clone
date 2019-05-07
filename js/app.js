c308b66cf80e6cfe2e2b456d2e89b4ee9549d763
Andy Jeong <unjin89@gmail.com>
2019-05-07 13:50:45 -0400

    created new variable gap; completed collision detection for both pipes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 // Variables
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let gap = 650;
+let pipeHeightDifference = 650;
 let birdX = 20;
 let birdY = 360;
 let gravity = 2;
 
 let pipeX = canvas.width;
 let pipeY = canvas.height - 300;
-let pipeTopY = pipeY - gap;
+let pipeTopY = pipeY - pipeHeightDifference;
+let gap = 150;
 let velocity = 2;
 
 const pipes = [
@@ -41,16 +42,14 @@ const drawMap = () => {
     
 
     for (i = 0; i < pipes.length; i++) {
-        ctx.drawImage(pipeTop, pipes[i].x, pipes[i].y - gap);
+        ctx.drawImage(pipeTop, pipes[i].x, pipes[i].y - pipeHeightDifference);
         ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y);
         pipes[i].x -= velocity;
         if (pipes[i].x === 150) {
-            console.log("new pipe")
             newPipe();
         }
-        if (pipes[i].x + pipeWidth === 0) {
+        if (pipes[i].x - pipeWidth === 0) {
             pipes.shift();
-            console.log(pipes);
         }
     }
     ctx.drawImage(ground, 0, 575, canvas.width, 100);
@@ -61,22 +60,20 @@ const drawMap = () => {
 drawMap();
 
 document.addEventListener('click', function flapWings() {
-    console.log('click');
     birdY -= 50;
 });
 
 
 const newPipe = () => {
-        newPipeY = (Math.floor(Math.random() * (500 - 200) + 200));
-        newPipeTopY = newPipeY - gap;
-        console.log(newPipeTopY);
+        pipeY = (Math.floor(Math.random() * (500 - 200) + 200));
+        pipeTopY = pipeY - pipeHeightDifference;
         pipes.push({
             x: pipeX,
-            y: newPipeY
+            y: pipeY
         });
         ctx.drawImage(pipeTop, pipeX, pipes[i].y);
         ctx.drawImage(pipeBottom, pipeX, pipes[i].y);
-        
+        console.log(pipes[i].y)
 }
 
 function gameOver() {
@@ -87,8 +84,10 @@ function gameOver() {
     }
     for (let k = 0; k < pipes.length; k++) {
         if (birdX + bird.width > pipes[k].x && birdX + bird.width < pipes[k].x + pipeTop.width) {
-            console.log("COLLISION WARNING");
-            document.removeEventListener('click', flapWings());
+            if (birdY + bird.height > pipes[k].y || birdY + bird.height  < pipes[k].y - gap) {
+                console.log("COLLISION WARNING TOP");
+                document.removeEventListener('click', flapWings());
+            } 
         }
     }
 }
