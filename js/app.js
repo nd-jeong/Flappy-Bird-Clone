12d3e01de692da8750944ec5b94b99f35de06038
Andy Jeong <unjin89@gmail.com>
2019-05-06 20:47:10 -0400

    added event listener: on click will increase the height of the bird

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let gap = 650;
-let birdX = 10;
+let birdX = 20;
 let birdY = 360;
 let gravity = 2;
 let pipeX = canvas.width;
@@ -12,6 +12,7 @@ let velocity = 2;
 
 const pipes = [];
 
+// https://www.codeexplained.org/2018/08/create-flappy-bird-game-using-javascript.html
 const bird = new Image();
 bird.src = 'images/bluebird-upflap.png';
 const pipeTop = new Image();
@@ -22,25 +23,29 @@ const bg = new Image();
 bg.src = 'images/background-night.png';
 const ground = new Image();
 ground.src = 'images/base.png';
+// --------------------------
+
+
 
 const drawMap = () => {
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(ground, 0, 575, canvas.width, 100);
     ctx.drawImage(bird, birdX, birdY);
     birdY += gravity;
-
+    
     ctx.drawImage(pipeTop, pipeX-100, pipeTopY);
     ctx.drawImage(pipeBottom, pipeX-100, pipeBottomY);
-    
+
     requestAnimationFrame(drawMap);
 }
 
 drawMap();
 
-document.addEventListener('click', flapBird());
-function flapBird() {
+document.addEventListener('click', function() {
+    console.log('click');
     birdY -= 25;
-}
+});
+
 
 const newPipe = () => {
     pipeBottomY = (Math.floor(Math.random() * (500 - 200) + 200));
