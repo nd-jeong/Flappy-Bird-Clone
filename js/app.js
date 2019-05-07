8c9af59069c30dc596d97af317c4bee567d865d7
Andy Jeong <unjin89@gmail.com>
2019-05-07 09:00:30 -0400

    added citation and increased the number of pixels that need to pass before creating new pipes

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let gap = 650;
 let birdX = 20;
 let birdY = 360;
 let gravity = 2;
+
 let pipeX = canvas.width;
 let pipeY = canvas.height - 300;
 let pipeTopY = pipeY - gap;
@@ -19,7 +20,7 @@ const pipes = [
 // https://www.codeexplained.org/2018/08/create-flappy-bird-game-using-javascript.html
 const bird = new Image();
 bird.src = 'images/bluebird-upflap.png';
-const pipeTop = new Image();
+let pipeTop = new Image();
 pipeTop.src = 'images/pipe-green-top.png';
 const pipeBottom = new Image();
 pipeBottom.src = 'images/pipe-green-bottom.png';
@@ -29,7 +30,8 @@ const ground = new Image();
 ground.src = 'images/base.png';
 // --------------------------
 
-
+let pipeWidth = pipeTop.width;
+// https://www.youtube.com/watch?v=cXgA1d_E-jY
 
 const drawMap = () => {
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
@@ -42,14 +44,15 @@ const drawMap = () => {
         ctx.drawImage(pipeTop, pipes[i].x, pipes[i].y - gap);
         ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y);
         pipes[i].x -= velocity;
-        if (pipes[i].x === 300) {
+        if (pipes[i].x === 150) {
+            console.log("new pipe")
             newPipe();
         }
+        if (pipes[i].x + pipeWidth === 0) {
+            setTimeout(pipes.splice(0, 1), 500);
+            console.log(pipes);
+        }
     }
-
-    // if (pipes[i].x === 300) {
-    //     newPipe();
-    // }
 
     requestAnimationFrame(drawMap);
 }
@@ -72,5 +75,4 @@ const newPipe = () => {
         });
         ctx.drawImage(pipeTop, pipeX, pipes[i].y);
         ctx.drawImage(pipeBottom, pipeX, pipes[i].y);
-        debugger;
 }
